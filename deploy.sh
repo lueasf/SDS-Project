dc0c9f15b0720679de93dd315f80f792d57e0695
Valdemar <valdemarbang@hotmail.com>
2025-05-06 15:30:27 +0200

    added ryu controller and requirements.txt

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+# Deploys the network topology through mininet
+sudo mn --custom network_topology.py --topo NetworkTopology --controller remote --switch ovsk --mac
 
-# Deploys the 
-sudo mn --custom network_topology.py --topo NetworkTopology --test pingall
+# Deploys the RYU controller running on the mininet
+ryu-manager ryu_controller.py

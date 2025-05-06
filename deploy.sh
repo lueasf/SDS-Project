58d61983a2f81807bdee65abfd87d4f26db67959
Valdemar <valdemarbang@hotmail.com>
2025-05-06 15:35:12 +0200

    god jul

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # Deploys the RYU controller running on the mininet
-ryu-manager ryu_controller.py
+ryu-manager ryu_controller.py &
 
 # Deploys the network topology through mininet
 sudo mn --custom network_topology.py --topo NetworkTopology --controller remote --switch ovsk --mac

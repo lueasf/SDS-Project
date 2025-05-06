75d3e93f0ea1b5659a4e73736d03d942077c5388
Valdemar <valdemarbang@hotmail.com>
2025-05-06 15:53:34 +0200

    added sleep

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@
 # Deploys the RYU controller in a new terminal for ubuntu
 gnome-terminal -- bash -c "ryu-manager ryu_controller.py; exec bash"
 
+# Sleep so RYU controller have time to start
+sleep 2
+
 # Deploys the network topology through mininet
 sudo mn --custom network_topology.py --topo NetworkTopology --controller remote --switch ovsk --mac

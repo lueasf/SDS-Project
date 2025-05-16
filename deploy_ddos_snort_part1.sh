3420a185abb8eeb742a0c234e543b1fc7b40fbea
plushayvan <ahmedibraheem2206@gmail.com>
2025-05-16 18:45:47 +0200

    fixed ddos part1 script

--- deploy_ddos_snort_part1.sh
+++ deploy_ddos_snort_part1.sh
@@ -14,4 +14,4 @@ sleep 5
 
 # adds the s1-snort link to switch s1
 # need to check port if s1-snort is the same as in ryu_controller_ddos_snort.py!
-gnome-terminal -- bash -c "sudo ovs-vsctl add-port s1 s1-snort; sudo ovs-ofctl show s1; echo "[*] check if port of s1-snort is the same as ryu_controller_ddos_snort.py! Then exceute script part2"; exec bash"
+gnome-terminal -- bash -c 'sudo ovs-vsctl add-port s1 s1-snort; sudo ovs-ofctl show s1; echo "[*] check if port of s1-snort is the same as ryu_controller_ddos_snort.py! Then exceute script part2"; exec bash'
